908c55bd8ca233ca186d94a2cc14b745c78af883
Lars Kappert <lars@webpro.nl>
2026-03-08 11:01:06 +0100

    Use Navigate component instead of imperative navigate in route

--- packages/web/src/routes/chart/index.tsx
+++ packages/web/src/routes/chart/index.tsx
@@ -1,6 +1,5 @@
-import { useNavigate } from '@solidjs/router';
+import { Navigate } from '@solidjs/router';
 
 export default function ChartPage() {
-  const navigate = useNavigate();
-  return navigate('/', { replace: true });
+  return <Navigate href="/" />;
 }
